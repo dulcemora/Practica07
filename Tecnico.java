77e7d2b908676b0eb0419af772f2a41253f7a7c0
dulcemora <dulcemora03@ciencias.unam.mx>
2021-12-07 19:28:05 -0600

    tecnico heredando operador

---
+++ Tecnico.java
@@ -0,0 +1,23 @@
+/**
+ *Clase 
+ * @author Dulce Mora
+ * @version 1.0
+**/
+public class Tecnico extends Operador{
+
+    /**
+     * Metodo constructor que recibe un String
+     * @param nombre Parametro que es el nombre
+     **/
+    public Tecnico(String nombre){
+	super(nombre);
+    }
+
+    /**
+     * Metodo constructor que da como nombre predeterminado Dulce Mora
+     **/
+    public Tecnico(){
+	super();
+    }
+
+}

fb0be09bf485acf62ad01034c15d4296aaaae50a
dulcemora <dulcemora03@ciencias.unam.mx>
2021-12-07 18:43:31 -0600

    Agregando bubbleSort

--- Sort.java
+++ Sort.java
@@ -1,33 +1,49 @@
 import java.util.Random;
 
 public class Sort{
-    
-    public static int particion(int arr2[], int izq, int der) {
-        int pivote = arr2[izq];
+
+    //Quicksort
+    public static int particion(int arr[], int izq, int der) {
+        int pivote = arr[izq];
 	while (true) {
-            while (arr2[izq] < pivote) {
+            while (arr[izq] < pivote) {
                 izq++;
             }
-            while (arr2[der] > pivote) {
+            while (arr[der] > pivote) {
                 der--;
             }
             if (izq >= der) {
                 return der;
             } else {
-                int temporal = arr2[izq];
-                arr2[izq] = arr2[der];
-                arr2[der] = temporal;
+                int temporal = arr[izq];
+                arr[izq] = arr[der];
+                arr[der] = temporal;
                 izq++;
                 der--;
             }
         }
     }
 
-    public static void quicksort(int arr2[], int izq, int der) {
+    public static void quicksort(int arr[], int izq, int der) {
     if (izq < der) {
-        int indiceParticion = particion(arr2, izq, der);
-        quicksort(arr2, izq, indiceParticion);
-        quicksort(arr2, indiceParticion + 1, der);
+        int indiceParticion = particion(arr, izq, der);
+        quicksort(arr, izq, indiceParticion);
+        quicksort(arr, indiceParticion + 1, der);
        }
     }
+
+    //Bubblesort
+    public static void bubblesort(int[] arr2) {
+	for(int i = 0; i < arr2.length; i++){
+	    for(int j = 0; j < arr2.length - 1; j++){
+		if (arr2[j] > arr2[j+1]){
+		    int temp = arr2[j];
+		    arr2[j] = arr2[j + 1];
+		    arr2[j + 1] = temp; 
+		}
+	    }
+	}
+    }
+ 
+
 }

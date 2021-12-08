36ea498dbeca3ac1858adb57ae5ba8b76eb795bb
dulcemora <dulcemora03@ciencias.unam.mx>
2021-12-07 19:02:02 -0600

    Agregando metodos

---
+++ Empleado.java
@@ -0,0 +1,51 @@
+/**
+ *Clase 
+ * @author Dulce Mora
+ * @version 1.0
+**/
+public class Empleado{
+
+    //Atributos
+    protected String nombre;
+
+    /**
+     * Metodo constructor que da como nombre predeterminado Dulce Mora
+     **/
+    public Empleado(){
+	this.nombre = "Dulce Mora";
+    }
+    
+    /**
+     * Metodo constructor que recibe un String
+     * @param nombre Parametro que es el nombre
+     **/
+    public Empleado(String nombre){
+	this.nombre = nombre;
+    }
+
+    /**
+     * Metodo que devuelve el nombre
+     * @return El nombre
+     **/
+    public String getNombre(){
+	return this.nombre;
+    }
+    /**
+     * Metodo que modifica el nombre
+     **/
+    public void setNombre(String nombre){
+	this.nombre = nombre;
+    }
+    
+    //toString
+    public String toString(){
+	return "Empleado: " + nombre;
+    }
+    
+    //equals
+    @Override
+    public boolean equals(Object a){
+	Empleado em = (Empleado) a;
+	return this.nombre.equals(em.getNombre());
+    }    
+}

6c7d62b7746c47155466a55ed4753f6d9c4a2655
dulcemora <dulcemora03@ciencias.unam.mx>
2021-12-07 19:11:00 -0600

    Operador heredando de empleado

---
+++ Operador.java
@@ -0,0 +1,19 @@
+public class Operador extends Empleado{
+
+    /**
+     * Metodo constructor que recibe un String
+     * @param nombre Parametro que es el nombre
+     **/
+    public Operador(String nombre){
+	super(nombre);
+    }
+
+    /**
+     * Metodo constructor que da como nombre predeterminado Dulce Mora
+     **/
+    public Operador(){
+	super();
+    }
+
+
+}

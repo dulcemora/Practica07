6aae42c697539998bbc860b59349aade01c75625
dulcemora <dulcemora03@ciencias.unam.mx>
2021-12-07 18:37:26 -0600

    Agregando quickSort

--- Sort.java
+++ Sort.java
@@ -1,3 +1,33 @@
-public class Sort{
+import java.util.Random;
 
+public class Sort{
+    
+    public static int particion(int arr2[], int izq, int der) {
+        int pivote = arr2[izq];
+	while (true) {
+            while (arr2[izq] < pivote) {
+                izq++;
+            }
+            while (arr2[der] > pivote) {
+                der--;
+            }
+            if (izq >= der) {
+                return der;
+            } else {
+                int temporal = arr2[izq];
+                arr2[izq] = arr2[der];
+                arr2[der] = temporal;
+                izq++;
+                der--;
+            }
+        }
+    }
+
+    public static void quicksort(int arr2[], int izq, int der) {
+    if (izq < der) {
+        int indiceParticion = particion(arr2, izq, der);
+        quicksort(arr2, izq, indiceParticion);
+        quicksort(arr2, indiceParticion + 1, der);
+       }
+    }
 }
